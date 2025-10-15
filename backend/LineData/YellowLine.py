9dae2d96af6b3f5fcc5afb027682b8ede5693fc7
MainframeKuznetSov <mainframe.comp.prog@gmail.com>
2025-10-15 21:01:12 +0530

    Yellow Line Data Uploaded

---
+++ backend/LineData/YellowLine.py
@@ -0,0 +1,5 @@
+yellow_line={"Noapara":[('035',0.0,0.0)],
+             "Dumdum Cantonment":[('053',2.835,3.7)],
+             "Jessore Road":[('054',2.29,4.7)],
+             "Jai Hind Bimanbandar":[('055',1.127,2.0)]
+            }
