67d5fa905cea876c930b3402eb23f5eae3526f98
MainframeKuznetSov <mainframe.comp.prog@gmail.com>
2025-10-15 20:56:13 +0530

    Orange Line Data Uploaded

---
+++ backend/LineData/OrangeLine.py
@@ -0,0 +1,10 @@
+orange_line={"Kavi Subhash":[('013',0.0,0.0)],
+             "Satyajit Ray":[('045',1.89,3.3)],
+             "Jyotirindra Nandy":[('046',1.393,3.5)],
+             "Kavi Sukanta":[('047',1.096,2.0)],
+             "Hemanta Mukhopadhyay":[('048',1.02,2.2)],
+             "VIP Bazar":[('049',1.282,4.3)],
+             "Ritwik Ghatak":[('050',0.924,2.3)],
+             "Barun Sengupta":[('051',1.251,4.8)],
+             "Beleghata":[('052',0.935,6.8)]
+            }
