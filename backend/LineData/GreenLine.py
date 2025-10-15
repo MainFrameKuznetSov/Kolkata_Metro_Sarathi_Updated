c716cead806eec4e58115aa78bbee1b593498b30
MainframeKuznetSov <mainframe.comp.prog@gmail.com>
2025-10-15 18:59:34 +0530

    Green Line Data uploaded

---
+++ backend/LineData/GreenLine.py
@@ -0,0 +1,13 @@
+green_line={"Howrah Maidan":[('001',0.0,0.0)],
+            "Howrah Station":[('002',1.057,2.5)],
+            "Mahakaran":['003',2.207,3.0],
+            "Esplanade":['004',0.878,2.0],
+            "Sealdah":['005',2.631,4.5],
+            "Phoolbagan":['006',2.306,2.7],
+            "Salt Lake Stadium":['007',1.699,2.5],
+            "Bengal Chemical":['008',0.795,1.3],
+            "City Centre I":['009',1.148,1.8],
+            "Central Park":['010',0.905,1.7],
+            "Karunamayee":['011',0.804,1.7],
+            "Salt Lake Sector V":['012',1.228,2.7]
+            }
